f8c50e590ec36a3ddef7e462a21efecdf1bafdd2
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-02-08 05:30:24 +0800

    change text content

---
+++ 002 BasicID/index.js
@@ -0,0 +1,11 @@
+
+// //print in console(dev tool > console)
+// console.log(`Hello`);
+// console.log(`I like pizza`);
+
+// //create alert box
+// window.alert(`This is an alert! `);
+// window.alert(`I like pizza `);
+
+document.getElementById("content-title").textContent = `Hello`;
+document.getElementById("content-entry").textContent = `loremm ipsumm`;
