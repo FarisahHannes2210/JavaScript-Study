a84d3fa7ac4ef63f35f6c749f50b9e0d773750b6
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-02-10 17:03:50 +0800

    logical operators

---
+++ 017 LogicalOperators/index.js
@@ -0,0 +1,13 @@
+//logical operators: 
+// AND = &&
+// OR = ||
+// NOT = !
+
+let temp = 28;
+
+if(temp > 0 && temp <= 30){
+    console.log("Weather is GOOD");
+}
+else{
+    console.log("Weather is BAD");
+}
