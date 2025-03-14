6cf9bc70f9701a4f720a3ecce606c24d099ed6b4
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-03-14 23:56:27 +0800

    Update index.js

--- 042 Inheritence/index.js
+++ 042 Inheritence/index.js
@@ -3,3 +3,40 @@
 // helps with code reusability
 
 
+class Animal{
+    alive = true;
+
+    eat(){
+        console.log(`This ${this.name} is eating.`);
+    }
+
+    sleep(){
+        console.log(`This ${this.name} is sleeping.`);
+    }
+
+}
+
+class Rabbit extends Animal{
+    name = "rabbit";
+    alive = true;
+}
+
+class Fish extends Animal{
+    name = "fish";
+    alive = false;
+}
+
+class Hawk extends Animal{
+    name = "hawk";
+    alive = true;
+}
+
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+console.log(rabbit.alive);
+rabbit.eat();
+fish.sleep();
+
+console.log(fish.alive);
