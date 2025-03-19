0547d4d9b62f42062cae98b5bf48637f31580f83
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-03-19 14:49:47 +0800

    nested objects

---
+++ 046 NestedObjects/index.js
@@ -0,0 +1,8 @@
+// nested objects = objects inside of other objects
+//                   allows you to represent more complex data structures
+//                  Child Object is enlosed by a Parent Object
+
+//      Person{Address{}, ContactInfo{}}
+//      ShoppingCart{Keyboard{}, Mouse{}, Monitor{}}
+
+
