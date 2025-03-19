566cbcdee22c3fa6ec9598af43e364fd9eca19d3
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-03-20 01:26:45 +0800

    array of objects

---
+++ 047 ArrayOfObjects/index.js
@@ -0,0 +1,15 @@
+const fruits = [
+  { name: "apple", color: "red", calories: 95 },
+  { name: "orange", color: "orange", calories: 657 },
+  { name: "banana", color: "yellow", calories: 345 },
+  { name: "strawberry", color: "red", calories: 34 },
+  { name: "peach", color: "pink", calories: 87 },
+];
+
+
+// print specific data
+console.log(fruits[2].name);
+
+// delete the last row
+// fruits.pop();
+
