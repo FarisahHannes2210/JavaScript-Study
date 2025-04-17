a406bf1f02dbfb0f17d390fdb8de87be2d56a885
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-04-18 00:15:00 +0800

    date object

--- 050 DateObjects/index.js
+++ 050 DateObjects/index.js
@@ -8,7 +8,7 @@ const date = new Date();
 
 
 //Date (year, month, day, hour, minute, second, ms)
-let customDate = new Date(2024, 0, 1, 2,3,4,5);
+let customDate = new Date(2001, 0, 1, 2,3,4,5);
 
 //given amount of time in ms since epic(when the date since my computer starts began)
 let dateBackThen = new Date(0);
@@ -71,3 +71,33 @@ console.log("hours: " + hours);
 console.log("minutes: " + minutes);
 console.log("seconds: " + seconds);
 console.log("day of the week: " + dayOfWeek);
+
+// -------------- we can set date too
+date.setFullYear(2024);
+date.setMonth(0);
+date.setDate(1);
+date.setHours(2);
+date.setMinutes(3);
+date.setSeconds(4);
+
+console.log(date);
+
+//-----can compare date too
+
+// usual date format is : YYYY-MM-DD
+let date1 = new Date("2023-12-31");
+let date2 = new Date("2024-01-01");
+
+if(date2 > date1){
+    console.log(`HAPPY NEW YEAR!`);
+}
+
+let birthday1 = new Date("2001-10-22");
+let birthday2 = new Date("1968-02-06");
+
+if(birthday1 > birthday2){
+    console.log("b1 is younger.");
+}
+else{
+    console.log("b1 is older.");
+}
