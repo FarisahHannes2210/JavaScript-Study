99f5c508f317a02aa8f6acefe02b475c56c718d7
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-04-17 23:55:26 +0800

    date objects

---
+++ 050 DateObjects/index.js
@@ -0,0 +1,22 @@
+// date objects = objects that contain values that represents
+//                dates and times.
+//                  these date objects can be changed and formatted
+
+
+//date now.
+const date = new Date();
+
+
+//Date (year, month, day, hour, minute, second, ms)
+let customDate = new Date(2024, 0, 1, 2,3,4,5);
+
+//given amount of time in ms since epic(when the date since my computer starts began)
+let dateBackThen = new Date(0);
+
+let trysomething = new Date(1700000000000);
+
+console.log(date);
+console.log(customDate);
+
+console.log(dateBackThen);
+console.log(trysomething);
