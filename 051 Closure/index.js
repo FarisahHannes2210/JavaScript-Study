3739fe294df11a66829965fe7517fc44059e9ff9
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-04-23 21:10:51 +0800

    closure

--- 051 Closure/index.js
+++ 051 Closure/index.js
@@ -1,2 +1,49 @@
 // closure = a func defined inside another func,
-//          the inner 
+//          the inner func has access to variables
+//          and scope of the otehr func.
+//          allow for private variables and state maintenance
+//          used frequently in JS frameworks: React, Vue, Angular
+
+
+//--------- example 1 ----------------
+
+function outer(){
+    let message = "Hello";
+
+    // this are considered private
+    function inner(){
+       console.log(message);
+    }
+
+
+    inner();
+}
+
+//cannot excess cuz it out of the scope
+message = "Goodbye";
+
+outer();
+
+//--------- example 2 ----------------
+
+function createCounter(){
+    let count = 0;
+
+    function increment(){
+  
+        count++;
+        console.log(`Count increased to ${count}`);
+    }
+
+    return {increment};
+
+}
+
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+counter.increment();
+
+//undefined cuz it cant read, its private.
+console.log(counter.count);
