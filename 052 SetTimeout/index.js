b64f6631157a239aac9663a98aaf40300f0d6cc3
FarisahHannes2210 <hannesfarisah@gmail.com>
2025-04-25 22:38:36 +0800

    Update index.js

--- 052 SetTimeout/index.js
+++ 052 SetTimeout/index.js
@@ -1,1 +1,10 @@
-//setTimeout() = func in js that allows
+//setTimeout() = func in js that allows you to schedule
+//               the execution of a func after an amount of time (miliseconds)
+//              Times are approximates (varies based on the workload of the JavaScript)
+
+//              setTimeout(callback, delay);
+
+
+function sayHello(){
+    
+}
